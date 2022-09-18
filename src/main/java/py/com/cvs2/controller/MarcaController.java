f91205531d584fee55dc0181f646f74c8c558827
unknown <you@example.com>
2022-09-18 18:47:55 -0400

    no se que paso

--- src/main/java/py/com/cvs2/controller/MarcaController.java
+++ src/main/java/py/com/cvs2/controller/MarcaController.java
@@ -7,29 +7,29 @@ import py.com.cvs2.model.Marca;
 
 public class MarcaController {
 
-	public List<Marca> listMarcas() {
-		MarcaDao marcaDAO = new MarcaDao();
-		return marcaDAO.list();
-	}
+    public List<Marca> listMarcas() {
+        MarcaDao marcaDAO = new MarcaDao();
+        return marcaDAO.list();
+    }
 
-	public Marca getMarcaById(Integer id) {
-		MarcaDao marcaDAO = new MarcaDao();
-		return marcaDAO.findById(id);
-	}
+    public Marca getMarcaById(Integer id) {
+        MarcaDao marcaDAO = new MarcaDao();
+        return marcaDAO.findById(id);
+    }
 
-	public Marca saveMarca(Marca marca) throws Exception {
-		MarcaDao marcaDao = new MarcaDao();
-		return marcaDao.save(marca);
-	}
+    public Marca saveMarca(Marca marca) throws Exception {
+        MarcaDao marcaDao = new MarcaDao();
+        return marcaDao.save(marca);
+    }
 
-	public Marca updateMarca(Marca marca) {
-		MarcaDao marcaDao = new MarcaDao();
-		return marcaDao.update(marca);
-	}
+    public Marca updateMarca(Marca marca) {
+        MarcaDao marcaDao = new MarcaDao();
+        return marcaDao.update(marca);
+    }
 
-	public void deleteMarca(Integer id) {
-		MarcaDao marcaDao = new MarcaDao();
-		marcaDao.delete(id);
-	}
+    public void deleteMarca(Integer id) {
+        MarcaDao marcaDao = new MarcaDao();
+        marcaDao.delete(id);
+    }
 
 }
