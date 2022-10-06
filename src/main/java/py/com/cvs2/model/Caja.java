b777ef0d69400382716ee2cf9f4ede8298701e64
René Azcurra <rene.az91@gmail.com>
2022-10-06 09:39:00 -0300

    Caja y AperturaCierreCaja

---
+++ src/main/java/py/com/cvs2/model/Caja.java
@@ -0,0 +1,64 @@
+package py.com.cvs2.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "caja")
+public class Caja implements Serializable {
+
+    private static final long serialVersionUID = -6533574310851771145L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "estado")
+    String estado;
+
+    public Caja() {
+    }
+
+    public Caja(int id, String descripcion, String estado) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Caja{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/py/com/cvs2/rest/AperturaCierreCajaRest.java
@@ -0,0 +1,79 @@
+package py.com.cvs2.rest;
+
+import py.com.cvs2.controller.AperturaCierreCajaController;
+import py.com.cvs2.model.AperturaCierreCaja;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/aperturacierrecaja")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class AperturaCierreCajaRest {
+
+    @GET
+    public Response listAperturaCierreCajas(@QueryParam("all") Boolean all) {
+        AperturaCierreCajaController mc = new AperturaCierreCajaController();
+        List<AperturaCierreCaja> aperturaCierreCajas = mc.listAperturaCierreCajas(all);
+
+        return Response.ok(aperturaCierreCajas, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getAperturaCierreCajaById(@PathParam("id") Integer id) {
+        AperturaCierreCajaController mc = new AperturaCierreCajaController();
+        AperturaCierreCaja aperturaCierreCaja = mc.getAperturaCierreCajaById(id);
+
+        return Response.ok(aperturaCierreCaja, MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    public Response saveAperturaCierreCaja(AperturaCierreCaja aperturaCierreCaja) {
+        AperturaCierreCajaController mc = new AperturaCierreCajaController();
+        try {
+            aperturaCierreCaja = mc.saveAperturaCierreCaja(aperturaCierreCaja);
+
+        } catch (Exception e) {
+            Response.ResponseBuilder rb = Response.status(Response.Status.BAD_REQUEST);
+            rb.entity(e.getMessage());
+            //throw new RuntimeException(e);
+            return rb.build();
+        }
+        return Response.ok(aperturaCierreCaja, MediaType.APPLICATION_JSON).build();
+    }
+
+    @PUT
+    public Response updateAperturaCierreCaja(AperturaCierreCaja aperturaCierreCaja) {
+        AperturaCierreCajaController mc = new AperturaCierreCajaController();
+        try {
+            aperturaCierreCaja = mc.updateAperturaCierreCaja(aperturaCierreCaja);
+        } catch (Exception e) {
+            Response.ResponseBuilder rb = Response.status(Response.Status.BAD_REQUEST);
+            rb.entity(e.getMessage());
+            //throw new RuntimeException(e);
+            return rb.build();
+        }
+
+        return Response.ok(aperturaCierreCaja, MediaType.APPLICATION_JSON).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteAperturaCierreCaja(@PathParam("id") Integer id) {
+        AperturaCierreCajaController mc = new AperturaCierreCajaController();
+
+        try {
+            mc.deleteAperturaCierreCaja(id);
+        } catch (Exception e) {
+            Response.ResponseBuilder rb = Response.status(Response.Status.BAD_REQUEST);
+            rb.entity(e.getMessage());
+            //throw new RuntimeException(e);
+            return rb.build();
+        }
+
+        return Response.ok().build();
+    }
+}
